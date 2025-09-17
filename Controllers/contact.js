c29a8478668dc5a4e883bad1c5d3247630e92cb6
CoolShivas <shivas2710cool00@gmail.com>
2025-09-17 13:52:06 +0530

    Checking the Controllers/contact.js file. Getting the console output and api response on Thunder Bolt/POSTMAN.

--- Controllers/contact.js
+++ Controllers/contact.js
@@ -1,1 +1,44 @@
-export const newContact = async (request, response) => {};
+export const newContact = async (request, response) => {
+  console.log("Printing the newContact => ", request.body);
+  response.json({
+    message: "New contact saved..",
+    data: request.body,
+    success: true,
+  });
+  // // // Open the Thunder Bolt or POSTMAN then select the request as POST enter the url (http://localhost:8000/api/contact/newcontact)
+  // // // Then, select the body and write this
+  /**
+   * {
+        "name":"batman",
+        "email":"batman@gmail.com",
+        "phone":"1234567890",
+        "type":"public"
+    }
+   */
+  // // // Finally, hit the send button of Thunder Bolt or POSTMAN. We will get the Output as :-
+  /**
+   *{
+      "message": "New contact saved..",
+        "data": {
+            "name": "batman",
+            "email": "batman@gmail.com",
+            "phone": "1234567890",
+            "type": "public"
+    },
+    "success": true
+}
+   */
+  // // // Therefore, Terminal Console Output as :-
+  /**
+   * Restarting 'app.js'
+Server is running at Port :-) 8000
+MongoDB Connected Successfully...!
+Printing the newContact =>  {
+  name: 'batman',
+  email: 'batman@gmail.com', 
+  phone: '1234567890',       
+  type: 'public'
+}
+
+   */
+};
