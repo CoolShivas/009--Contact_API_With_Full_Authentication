d2121b4bb22e2ae9a3a9bc8790a0abfe20beaa51
CoolShivas <shivas2710cool00@gmail.com>
2025-09-17 12:57:26 +0530

    Formation of Models ContactSCHEMA and connected with MongoDB  to automatically generate and manage the schema in your database.

---
+++ Models/ContactSCHEMA.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const contactSchema = new mongoose.Schema({
+  contactName: { type: String, require: true },
+  contactEmail: { type: String, require: true },
+  contactPhone: { type: Number, require: true },
+  contactType: { type: String, require: true },
+  createdAt: { type: Date, default: Date.now },
+});
+
+export const ContactSCHEMA = mongoose.model("contact", contactSchema);
