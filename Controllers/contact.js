a386818f8e588a3052ba47ccbc0b9b9f5ef96f55
CoolShivas <shivas2710cool00@gmail.com>
2025-09-17 16:29:34 +0530

    Adding the functionality to Controllers/contact.js i.e., Getting all the contacts from database. Also, add the get route on Routes/contact.js file.

--- Controllers/contact.js
+++ Controllers/contact.js
@@ -1,5 +1,65 @@
 import { ContactSCHEMA } from "../Models/ContactSCHEMA.js";
 
+// // // Getting all the contacts;
+export const getAllContacts = async (request, response) => {
+  const gettingContacts = await ContactSCHEMA.find();
+
+  if (!getAllContacts)
+    return response.json({ message: "No contact found", success: false });
+
+  response.json({
+    message: "Fetching all contacts",
+    success: true,
+    gettingContacts,
+  });
+  // // // Open the Thunder Bolt or POSTMAN and select the GET request and enter the url (http://localhost:8000/api/contact) and hit send button. You will get the response as :-
+  /**
+ * {
+  "message": "Fetching all contacts",
+  "success": true,
+  "gettingContacts": [
+    {
+      "_id": "68ca86f19afbab626760f7ed",
+      "contactName": "batman",
+      "contactEmail": "batman@gmail.com",
+      "contactPhone": 1234567890,
+      "contactType": "public",
+      "createdAt": "2025-09-17T10:01:21.249Z",
+      "__v": 0
+    },
+    {
+      "_id": "68ca8e3169a835b942296d89",
+      "contactName": "superman",
+      "contactEmail": "superman@gmail.com",
+      "contactPhone": 1234567890,
+      "contactType": "private",
+      "createdAt": "2025-09-17T10:32:17.382Z",
+      "__v": 0
+    },
+    {
+      "_id": "68ca903f803785e664141637",
+      "contactName": "wonderwomen",
+      "contactEmail": "wonderwomen@gmail.com",
+      "contactPhone": 9874563210,
+      "contactType": "private",
+      "createdAt": "2025-09-17T10:41:03.710Z",
+      "__v": 0
+    },
+    {
+      "_id": "68ca906a803785e664141639",
+      "contactName": "blackpanther",
+      "contactEmail": "blackpanther@gmail.com",
+      "contactPhone": 9873210456,
+      "contactType": "public",
+      "createdAt": "2025-09-17T10:41:46.153Z",
+      "__v": 0
+    }
+  ]
+}
+ */
+  console.log("Printing all contacts => ", gettingContacts); // // Getting the same Output on Terminal;
+};
+
 // // // Creating the new contact;
 export const newContact = async (request, response) => {
   //   console.log("Printing the newContact => ", request.body);
--- Routes/contact.js
+++ Routes/contact.js
@@ -1,11 +1,16 @@
 import express from "express";
-import { newContact } from "../Controllers/contact.js";
+import { getAllContacts, newContact } from "../Controllers/contact.js";
 
 const router = express.Router();
-// // // User Routes
+// // // New Contact
 // // // @api description :- create contact
 // // // @api method :- post
 // // // @api endPoint :- /api/contact/newcontact
 router.post("/newcontact", newContact);
+// // // Get all contacts
+// // // @api description :- get all contact
+// // // @api method :- get
+// // // @api endPoint :- /api/contact/ i.e, (http://localhost:8000/api/contact/)
+router.get("/", getAllContacts);
 
 export default router;
