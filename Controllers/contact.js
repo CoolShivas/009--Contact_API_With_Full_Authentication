89d8fd01e6d74e2e5a21eea417dd5d8b93033999
CoolShivas <shivas2710cool00@gmail.com>
2025-09-17 16:01:03 +0530

    Adding the functionality to Controllers/contact.js i.e., Creating the new contact function.

--- Controllers/contact.js
+++ Controllers/contact.js
@@ -1,33 +1,14 @@
+import { ContactSCHEMA } from "../Models/ContactSCHEMA.js";
+
+// // // Creating the new contact;
 export const newContact = async (request, response) => {
-  console.log("Printing the newContact => ", request.body);
-  response.json({
-    message: "New contact saved..",
-    data: request.body,
-    success: true,
-  });
-  // // // Open the Thunder Bolt or POSTMAN then select the request as POST enter the url (http://localhost:8000/api/contact/newcontact)
-  // // // Then, select the body and write this
-  /**
-   * {
-        "name":"batman",
-        "email":"batman@gmail.com",
-        "phone":"1234567890",
-        "type":"public"
-    }
-   */
-  // // // Finally, hit the send button of Thunder Bolt or POSTMAN. We will get the Output as :-
-  /**
-   *{
-      "message": "New contact saved..",
-        "data": {
-            "name": "batman",
-            "email": "batman@gmail.com",
-            "phone": "1234567890",
-            "type": "public"
-    },
-    "success": true
-}
-   */
+  //   console.log("Printing the newContact => ", request.body);
+  //   response.json({
+  //     message: "New contact saved..",
+  //     data: request.body,
+  //     success: true,
+  //   });
+
   // // // Therefore, Terminal Console Output as :-
   /**
    * Restarting 'app.js'
@@ -40,5 +21,37 @@ Printing the newContact =>  {
   type: 'public'
 }
 
+   */
+
+  const { name, email, phone, type } = request.body;
+
+  if (name === "" || email === "" || phone === "" || type === "")
+    return response.json({
+      message: "All fields are required",
+      success: false,
+    });
+
+  // // // Saving the data on DataBase;
+  const saveContact = await ContactSCHEMA.create({
+    contactName: name,
+    contactEmail: email,
+    contactPhone: phone,
+    contactType: type,
+  });
+  response.json({ message: "Contact saved to DB", success: true, saveContact });
+  console.log("Printing the saveContact => ", saveContact);
+  /**
+   * Restarting 'app.js'
+Server is running at Port :-) 8000
+MongoDB Connected Successfully...!
+Printing the saveContact =>  {
+  contactName: 'batman',
+  contactEmail: 'batman@gmail.com',
+  contactPhone: 1234567890,
+  contactType: 'public',
+  _id: new ObjectId('68ca85d9d66d13874ba7fa3f'),
+  createdAt: 2025-09-17T09:56:41.363Z,
+  __v: 0
+}
    */
 };
--- Routes/contact.js
+++ Routes/contact.js
@@ -2,7 +2,10 @@ import express from "express";
 import { newContact } from "../Controllers/contact.js";
 
 const router = express.Router();
-
+// // // User Routes
+// // // @api description :- create contact
+// // // @api method :- post
+// // // @api endPoint :- /api/contact/newcontact
 router.post("/newcontact", newContact);
 
 export default router;
